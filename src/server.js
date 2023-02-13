a554b2f68e2163855cc6dffcbf90ccd6a78f2e56
penny jiang <pennyjiang@pennys-MacBook.local>
2023-02-14 00:13:36 +1300

    upadated

--- src/server.js
+++ src/server.js
@@ -2,12 +2,15 @@ const express = require('express');
 const router = express.Router();
 const cors = require('cors');
 const nodemailer = require('nodemailer');
+const PORT = 5000;
+const EMAIL_USER = 'peijiang925@gmail.com';
+const EMAIL_PASS = 'unvuxbcawrpiauxk';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/', router);
-app.listen(5000, () => {
+app.listen(PORT, () => {
   console.log(`Server Running at http://localhost:5000`);
 });
 
@@ -17,8 +20,8 @@ console.log(process.env.EMAIL_PASS);
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'peijiang925@gmail.com',
-    pass: '1989925penny',
+    user: EMAIL_USER,
+    pass: EMAIL_PASS,
   },
 });
 
